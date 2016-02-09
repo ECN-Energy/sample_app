37bc8a00f50526906848aabf0ec6254e4bade16a
ECN-Energy <soogunfowora@gmail.com>
2016-02-09 16:01:24 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Ruby on Rails Tutorial: Sample Application
+
+This is the sample application for
+
+[*Ruby on Rails Tutorial: Learn Rails by Example*] (http://railstutorial.org/)
+by [Micheal Hart1] (http://michealhart1.com/)
+
+and learned by [Segun Ogunfowora]. 
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '5833128d9470266fec2da620aa6deeffc942387bb57f28cbd659c9d2a7975fb0528140d4eeb6c6157099f6135ca922ab982f12331ddf562f1a061dbe8001afa1'
